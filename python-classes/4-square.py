682d0d0ba81685efcd737820fd95970b18018b80
AllenKeelig <alallen.keeling@atlasschool.com>
2024-05-11 02:00:37 -0500

    1

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -16,7 +16,7 @@ class Square:
                 self.__size = size
 
     def size(self):
-        return self.__size
+        return (self.__size)
 
     def area(self):
         return self.__size ** 2
