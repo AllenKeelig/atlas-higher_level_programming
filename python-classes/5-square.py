b6b1180414d5dddba7f62e931a66a3cb675856b0
AllenKeelig <alallen.keeling@atlasschool.com>
2024-05-11 04:14:25 -0500

    1

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -22,6 +22,19 @@ class Square:
             else:
                 self.__size = val
 
+    @property
+    def position(self):
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if type(value) is tuple and len(value) is 2 and \
+            type(value[0]) is int and type(value[1]) is int and \
+                value[0] >= 0 and value[1] >= 0:
+            self.__position = value
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
     def area(self):
         return self.__size ** 2
 
