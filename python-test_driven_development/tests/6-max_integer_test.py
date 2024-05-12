4e0dc83c617fabd5781c7fd9d184a0754749478e
AllenKeelig <alallen.keeling@atlasschool.com>
2024-05-12 04:02:01 -0500

    4

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -10,3 +10,7 @@ class TestMaxInteger(unittest.TestCase):
         """Tests for all positive with max at end"""
         e = [2, 10, 8, 36, 14, 50]
         self.assertEqual(max_integer(e), 50)
+    def test_max_at_begginning(self):
+        """Test a list with a beginning max value."""
+        max_at_beginning = [4, 3, 2, 1]
+        self.assertEqual(max_integer(max_at_beginning), 4)
