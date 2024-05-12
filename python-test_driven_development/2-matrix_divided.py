f11e3b3685089eb6db3740779f9cebc973bbb9d4
AllenKeelig <alallen.keeling@atlasschool.com>
2024-05-12 02:06:21 -0500

    0

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) / int(b))
