5cc42648e253c728bf9b81954f043485c8c42434
AllenKeelig <allen.keeling@atlasschool.com>
2024-05-13 19:23:20 -0500

    10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     # Create an empty list to store the results
     result = []
-    
+
     # Iterate through each element in the input list
     for num in my_list:
         # Check if the number is divisible by 2
@@ -10,6 +10,6 @@ def divisible_by_2(my_list=[]):
             result.append(True)
         else:
             result.append(False)
-    
+
     # Return the list of True/False values
     return result
