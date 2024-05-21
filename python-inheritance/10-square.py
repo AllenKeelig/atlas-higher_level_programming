5a814d4da6a1839a83d5ca75e858a2509b840ce3
AllenKeelig <allen.keeling@atlasschool.com>
2024-05-21 10:21:18 -0500

    10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -3,10 +3,10 @@
 Contains subclass Square that inherits from Rectangle
 """
 
-BaseGeometry = __import__('9-rectangle.py').Rectangle
+Rectangle = __import__('9-rectangle.py').Rectangle
 
 
-class Square(BaseGeometry):
+class Square(Rectangle):
     """A rectangle"""
     def __init__(self, size):
         """Initialize a square with a given size"""
