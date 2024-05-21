1717b6d24d1de339fca5f0000e5698f5e0644f3a
AllenKeelig <allen.keeling@atlasschool.com>
2024-05-21 10:16:31 -0500

    10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -8,7 +8,6 @@ BaseGeometry = __import__('9-rectangle.py').Rectangle
 
 class Square(BaseGeometry):
     """A rectangle"""
-    def __init__(self, width, height):
-        def __init__(self, size):
+    def __init__(self, size):
         """Initialize a square with a given size"""
         self.__size = size
