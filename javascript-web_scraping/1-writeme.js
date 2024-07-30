320da0752962b4223942228c34f669a211e73887
AllenKeelig <allen.keeling@atlasschool.com>
2024-07-30 16:32:21 -0500

    1 semistandard validation comments

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-//feels wierd not having these be mandatory
+//feels wierd not having these be mandatory 
+
 const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[3], error => {
   if (error) console.log(error);
